daa45485cd823224c4f773dc258bbd8fdbf780ae
OlathedevGuy <jolamideprince@gmail.com>
2024-07-19 23:25:33 +0100

    first commit

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,52 @@
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
+
+const HeroSection = () => {
+  return (
+    <>
+        <div className="flex flex-col items-center mt-6 lg:mt-20">
+            <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+                VirtualR build mode  
+                <span className='bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text'>
+                {" "}for developers
+                </span>
+            </h1>
+            <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+            Empower your creativity and bring your VR app ideas to life with our
+            intuitive development tools. Get started today and turn your imagination
+            into immersive reality!
+            </p>
+            <div className="flex justify-center my-10">
+                <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 rounded-md'>
+                    Start for free 
+                </a>
+                <a href="#" className=' py-3 px-4 mx-3 rounded-md border'>
+                    Documentation 
+                </a>
+            </div>
+            <div className='flex mt-10 justify-center items-center'>
+
+                <video src={video1}
+                autoPlay 
+                loop 
+                muted 
+                className='rounded-lg w-1/2 border border-orange-700 mx-2 my-4 shadow-orange-950' >
+                    <source  type = 'video'/>
+                    -oops, your browser does not support this feature
+                </video>
+
+                <video src={video2}
+                autoPlay
+                 loop 
+                 muted 
+                 className='rounded-lg w-1/2 border border-orange-700 mx-2 my-4 shadow-orange-950' >
+                    <source  type = 'video'/>
+                    -oops, your browser does not support this feature
+                </video>
+
+            </div>
+        </div>
+    </>
+  )
+}
+export default HeroSection
